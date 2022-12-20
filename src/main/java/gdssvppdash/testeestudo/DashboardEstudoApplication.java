ab5b8115155dbda086d036e201719d96165641ee
gdssvpp <92610124+gdssvpp@users.noreply.github.com>
2022-12-20 16:23:52 -0300

    commit
    chore: primeiros arquivos adicionados

---
+++ src/main/java/gdssvppdash/testeestudo/DashboardEstudoApplication.java
@@ -0,0 +1,13 @@
+package gdssvppdash.testeestudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardEstudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gdssvppdash/testeestudo/DashboardEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package gdssvppdash.testeestudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
